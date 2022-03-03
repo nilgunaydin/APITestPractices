9b765bd6d2411cbb4d8b4c3f1883c54d7600d068
Nilgun <94299742+nilgunaydin@users.noreply.github.com>
2022-03-03 13:14:40 +0300

    first commit

---
+++ src/test/java/get_http_request/GetRequest01.java
@@ -0,0 +1,22 @@
+package get_http_request;
+
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetRequest01 {
+
+    @Test
+    public void test01(){
+        String url = "https://restful-booker.herokuapp.com/booking";
+      Response rs =   given().when().get(url);
+rs.prettyPrint();
+
+Assert.assertEquals(200,rs.statusCode());
+
+
+rs.then().assertThat().statusCode(200);
+    }
+}
